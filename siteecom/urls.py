ef32aa876ddbd12254122565896ea17ebcd0349b
Romain <roma.poisson@gmail.com>
2025-01-21 20:01:56 +0100

    ecom

--- siteecom/urls.py
+++ siteecom/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('templates/products/', views.product_list, name='product_list'),
     # path('nombre_de_taches', views.number_of_task, name='number_of_task'),
 ]
